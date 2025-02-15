cfa4ded4edc4403b4f866cc6b5ceb3e19a6b2ff7
alexxeyy <phoenix332228@gmail.com>
2025-02-15 17:58:44 +0300

    controller

---
+++ XO/backend/games.server/Game/Controllers/XOController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Game.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class XOController
+    {
+    }
+}
--- XO/backend/games.server/Game/obj/Debug/net8.0/Game.AssemblyInfo.cs
+++ XO/backend/games.server/Game/obj/Debug/net8.0/Game.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Game")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6cb442571e4c0251a994bd4211a4755098ab183")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b130932d49d100160e0bde8a8dadfd787992395")]
 [assembly: System.Reflection.AssemblyProductAttribute("Game")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Game")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
